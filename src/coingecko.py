d7bcde2d404332f7aa039d9b266895c1e31062c5
Chi Zhang <zhangchitc@gmail.com>
2024-11-21 16:35:18 +0800

    fix

--- src/coingecko.py
+++ src/coingecko.py
@@ -1,16 +1,19 @@
 from pycoingecko import CoinGeckoAPI
 
-client = CoinGeckoAPI()
+## As we are using a free API key, we need to change Coingecko API to use non-pro API endpoint
+## 'https://api.coingecko.com/api/v3/' with demo API key header 'x-cg-demo-api-key'
+client = CoinGeckoAPI(api_key=os.getenv("COINGECKO_API_KEY"), retries=1)
 
 
 def get_coins(*ids, currency: str = "usd"):
     currency = currency.lower()
 
+    res = []
     for coin in client.get_coins_markets(currency, ids=",".join(ids), per_page=20):
         if not coin["market_cap_rank"]:
             coin["market_cap_rank"] = -1
 
-        yield {
+        res.append({
             "id": coin["id"],
             "symbol": coin["symbol"],
             "name": coin["name"],
@@ -23,7 +26,9 @@ def get_coins(*ids, currency: str = "usd"):
             "atl_change": coin["atl_change_percentage"],
             "ath": coin["ath"],
             "ath_change": coin["ath_change_percentage"],
-        }
+        })
+
+    return res
 
 
 def search(symbols: set = None, currency: str = "usd") -> list:
